f2a161bba751a8e1b25a3613fb4623e34516122d
Mikhail Vialov <mikhail_vialov@epam.com>
2022-11-21 14:42:37 +0100

    clean up

--- lua/accme/core/colorscheme.lua
+++ lua/accme/core/colorscheme.lua
@@ -1,19 +1,3 @@
 vim.cmd("colorscheme gruvbox")
--- vim.cmd("highlight Normal ctermbg=NONE guibg=NONE")
-
-vim.cmd("highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080")
-vim.cmd("highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6")
-vim.cmd("highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6")
-vim.cmd("highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE")
-vim.cmd("highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE")
-vim.cmd("highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE")
-vim.cmd("highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0")
-vim.cmd("highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0")
-vim.cmd("highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4")
-
--- highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6
--- highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6
--- highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0
--- highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0
--- highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE
--- highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4
+vim.cmd("highlight Normal ctermbg=NONE guibg=NONE")
+vim.cmd("highlight! Pmenu guibg=#4c4543 ctermbg=NONE gui=strikethrough")
