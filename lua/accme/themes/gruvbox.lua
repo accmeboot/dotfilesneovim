864f9982d3675008482dd1bf2d29bbe902795d9f
accme <mishavjalv48@gmail.com>
2024-03-10 12:56:51 +0100

    status line clean up

--- lua/accme/themes/gruvbox.lua
+++ lua/accme/themes/gruvbox.lua
@@ -2,6 +2,6 @@ vim.g.gruvbox_material_foreground = "mix" -- material, mix, original
 vim.g.gruvbox_material_better_performance = 1
 vim.g.gruvbox_material_enable_bold = 1
 vim.g.gruvbox_material_transparent_background = 1 -- 0, 1 or 2
-vim.g.gruvbox_material_statusline_style = "mix" -- "original", "default", "mix"
+vim.g.gruvbox_material_statusline_style = "default" -- "original", "default", "mix"
 
 vim.cmd("colorscheme gruvbox-material")
