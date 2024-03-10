26c3cec0c6f356ba69a5f19800bf7fb8f1d5fcf0
Mikhail Vialov <mikhail_vialov@epam.com>
2024-03-10 11:34:33 +0100

    removed underline

--- lua/accme/core/colorscheme.lua
+++ lua/accme/core/colorscheme.lua
@@ -24,4 +24,5 @@ vim.cmd("highlight link NormalFloat Normal")
 vim.cmd("highlight NormalFloat ctermbg=NONE guibg=NONE")
 vim.cmd("highlight Pmenu ctermbg=NONE guibg=NONE")
 
-vim.api.nvim_set_hl(0, "CursorLine", { underline = true })
+-- make cursorline underlined
+-- vim.api.nvim_set_hl(0, "CursorLine", { underline = true })
