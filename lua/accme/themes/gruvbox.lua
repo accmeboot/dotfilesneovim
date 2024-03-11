ebc117669f01db9cd7bc5bd5fec0608dbd0eebf0
Mikhail Vialov <mikhail_vialov@epam.com>
2024-03-11 16:28:52 +0100

    better diagnostics highliht

--- lua/accme/themes/gruvbox.lua
+++ lua/accme/themes/gruvbox.lua
@@ -3,5 +3,6 @@ vim.g.gruvbox_material_better_performance = 1
 vim.g.gruvbox_material_enable_bold = 1
 vim.g.gruvbox_material_transparent_background = 1 -- 0, 1 or 2
 vim.g.gruvbox_material_statusline_style = "default" -- "original", "default", "mix"
+vim.g.gruvbox_material_diagnostic_virtual_text = "highlighted" -- "grey", "colored", "highlighted"
 
 vim.cmd("colorscheme gruvbox-material")
