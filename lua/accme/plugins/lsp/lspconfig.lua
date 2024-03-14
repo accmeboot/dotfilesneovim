4f2dd4bbb301dfdb40d8f96b7f91da8f1be7614c
Mikhail Vialov <mikhail_vialov@epam.com>
2024-03-14 23:25:35 +0100

    vurtual text sign fix

--- lua/accme/plugins/lsp/lspconfig.lua
+++ lua/accme/plugins/lsp/lspconfig.lua
@@ -112,9 +112,8 @@ return {
 			},
 		})
 
-		vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+		vim.diagnostic.config({
 			virtual_text = {
-				-- prefix = " ",
 				prefix = "",
 				spacing = 4,
 			},
