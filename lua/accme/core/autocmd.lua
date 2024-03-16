bc3fb9f01df7a45a8d4eb4a16e5c904710aebf02
Mikhail Vialov <mikhail_vialov@epam.com>
2024-03-17 00:55:45 +0100

    back to normal

--- lua/accme/core/autocmd.lua
+++ lua/accme/core/autocmd.lua
@@ -1,3 +1,1 @@
--- this add the space at the line numbers column
-vim.cmd("set number relativenumber") -- Set relative numbers and absolute numbers for the current line
-vim.cmd("let &statusline=' %C %s%l%r'") -- Add a space before signs
+vim.cmd("let &statuscolumn='%C %s%l%r'")
