0fcd0e3fb2e67212f2de6cb19ed0a780f88bc427
Mikhail Vialov <mikhail_vialov@epam.com>
2024-03-17 12:00:35 +0100

    better lualine & statuscolumn (finally)!

--- lua/accme/core/autocmd.lua
+++ lua/accme/core/autocmd.lua
@@ -1,1 +1,2 @@
-vim.cmd("let &statuscolumn='%C %s%l%r'")
+-- vim.cmd("let &statuscolumn='%C %s%l%r │ %T'")
+vim.cmd("let &statuscolumn='%C %s%{v:relnum?v:relnum:v:lnum} '")
--- lua/accme/plugins/lualine.lua
+++ lua/accme/plugins/lualine.lua
@@ -5,9 +5,28 @@ return {
 		require("lualine").setup({
 			sections = {
 				lualine_a = {},
-				lualine_b = { "branch", "diff" },
-				lualine_c = { "filename" },
-				lualine_x = { "filetype", "diagnostics" },
+				lualine_b = {
+					{
+						"branch",
+						component_separators = { left = "»", right = "«" },
+						icon = "⎇`",
+					},
+					"diff",
+				},
+				lualine_c = {
+					{
+						"filename",
+						component_separators = { left = "»", right = "«" },
+					},
+					{
+						"diagnostics",
+						symbols = { error = "E:", warn = "W:", info = "I:", hint = "H:" },
+						component_separators = { left = "»", right = "«" },
+					},
+				},
+				lualine_x = {
+					"filetype",
+				},
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
 			},
