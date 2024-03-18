d6aca618b55bf19ff9a4159e48991ebfe4144165
Mikhail Vialov <mikhail_vialov@epam.com>
2024-03-18 20:15:39 +0100

    removed extra snippets

--- lua/accme/plugins/nvim-cmp.lua
+++ lua/accme/plugins/nvim-cmp.lua
@@ -4,20 +4,12 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
-		"L3MON4D3/LuaSnip", -- snippet engine
 		"saadparwaiz1/cmp_luasnip", -- for autocompletion
-		"rafamadriz/friendly-snippets", -- useful snippets
 		"onsails/lspkind.nvim", -- vs-code like pictograms
 	},
 	config = function()
 		local cmp = require("cmp")
-
-		local luasnip = require("luasnip")
-
 		local lspkind = require("lspkind")
-
-		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-		require("luasnip.loaders.from_vscode").lazy_load()
 
 		cmp.setup({
 			window = {
@@ -33,11 +25,6 @@ return {
 			completion = {
 				completeopt = "menu,menuone,preview,noselect",
 			},
-			snippet = { -- configure how nvim-cmp interacts with snippet engine
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
@@ -52,7 +39,6 @@ return {
 				{ name = "nvim_lsp" },
 				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
-				{ name = "luasnip" }, -- snippets
 			}),
 			sorting = {
 				priority_weight = 2, -- prioritize by source
